51dce9b2d95fda733dab048192de59b9e2c9404f
Emeka Ifeora <34845973+Emyboy@users.noreply.github.com>
2025-11-27 02:47:51 +0100

    Enhance blog and FAQ sections; integrate dynamic data and improve layout

--- src/site/app/handler.ts
+++ src/site/app/handler.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express';
 import type { SiteMetadata } from '../../types/app.types.js';
+import { iDeginCloud } from '../../lib/idegin-cloud.js';
 
 export default async function handler(req: Request, res: Response) {
+
+    const faq = await iDeginCloud('/public/cms/collections/faq');
+    const blogPosts = await iDeginCloud('/public/cms/collections/blog-posts?limit=3');
+
+    console.log('data:::', {
+        data: blogPosts.data.entries[0]?.data,
+        author: blogPosts.data.entries[0]?.data.author,
+        thumbnail: blogPosts.data.entries[0]?.data.thumbnail,
+        categories: blogPosts.data.entries[0]?.data.categories[0]?.data,
+    })
+    
     return {
         data: {
-            
+            faq: faq.data.entries,
+            blogPosts: blogPosts.data.entries
         },
         metadata: {
             title: 'The Plug Afrique | Strategic Consulting for Impact in Africa',
